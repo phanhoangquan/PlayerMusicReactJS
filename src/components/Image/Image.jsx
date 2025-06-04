b1c36cc33cb44dd210b6f02ac76027bae46a60d8
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-06-05 00:33:41 +0700

    Fix Error Image no update useState when src changed

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,12 +1,15 @@
 import classNames from 'classnames/bind';
 import styles from './Image.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import images from '~/assets/images/images';
 
 const cx = classNames.bind(styles);
 
 function Image({ src, alt = '', className, ...props }) {
    const [avatar, setAvatar] = useState(src);
+   useEffect(() => {
+      setAvatar(src);
+   }, [src]);
 
    const handleError = () => {
       setAvatar(images.noimage);
